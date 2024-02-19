513ef405b20638cde5227fb12187b60d91f7701a
neochentwhia <neochentwhia@gmail.com>
2024-02-19 08:09:17 +0800

    建立專案

--- MyWebsite/MyWebsite/urls.py
+++ MyWebsite/MyWebsite/urls.py
@@ -14,7 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+import django.contrib.auth.views as auth_view
+from . import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
