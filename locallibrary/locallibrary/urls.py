88556ae4f9c0d396c07d88c211280a2b26b0f527
neochentwhia <neochentwhia@gmail.com>
2024-02-19 08:39:44 +0800

    專案建立

--- locallibrary/locallibrary/urls.py
+++ locallibrary/locallibrary/urls.py
@@ -1,4 +1,4 @@
-"""MyWebsite URL Configuration
+"""locallibrary URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
@@ -14,10 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-import django.contrib.auth.views as auth_view
-from . import views
-
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
